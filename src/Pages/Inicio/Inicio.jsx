2ba24e62cbee61292e6b76bc5f6f201cd4326986
Glaudia <glaudiaalmeida@gmail.com>
2024-11-30 20:55:52 -0300

    concluindo page Inicio

--- src/Pages/Inicio/Inicio.jsx
+++ src/Pages/Inicio/Inicio.jsx
@@ -1,10 +1,37 @@
 import S from "./inicio.module.scss"
+import community from "../../assets/community.png"
+import reading from "../../assets/reading.png"
+import transform from "../../assets/transform.png"
+import balance from "../../assets/balance.png"
 
 export default function Inicio() {
     return(
         <section>
-            <section className={S.post}>
+            <section className={S.banner}>
                 <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
+            </section>
+            <section className={S.devoDoar}>
+                <div>
+                    <h2>Por que devo doar?</h2>
+                </div>
+                <section className={S.boxCard}>
+                    <article>
+                        <img src={community} alt="Imagem representado uma comunidade."/>
+                        <p>Oferece Livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
+                    </article>
+                    <article>
+                        <img src={reading} alt="Imagem de um bonequinho lendo um livro."/>
+                        <p>Estimula o hábito da leitura e o aprendizado contínuo.</p>
+                    </article>
+                    <article>
+                        <img src={transform} alt="Imagem representando várias pessoas fortalecidas pela leitura."/>
+                        <p>Fornece conhecimento e inspiração, permitindo que indíviduos transformem suas vidas.</p>
+                    </article>
+                    <article>
+                        <img src={balance} alt="Imagem de uma balança simbolo do direito."/>
+                        <p>Garante que todos, independente de sua condição, tenham oportunidades de aprendizado.</p>
+                    </article>
+                </section>
             </section>
         </section>
     )
--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,10 +1,26 @@
+import S from "./footer.module.scss"
+import facebook from "../../assets/facebook.png"
+import twitte from "../../assets/twitte.png"
+import youtube from "../../assets/youtube.png"
+import linkedin from "../../assets/linkedin.png"
+import instagram from "../../assets/instagram.png"
 
 
 export default function Footer() {
     return(
         <footer>
-            <section>
-              <h1>Criado por Vai na Web</h1>
+            <section className={S.contactIcons}>
+                <h3>4002-8922</h3>
+                <nav>
+                    <a href="https://www.facebook.com/"><img src={facebook} alt="Logo da rede social Facebook"/></a>
+                    <a href="https://x.com/"><img src={twitte} alt="Logo da rede social x"/></a>
+                    <a href="https://www.youtube.com/"><img src={youtube} alt="Logo da rede social Youtube"/></a>
+                    <a href="https://www.linkedin.com/"><img src={linkedin} alt="Logo da rede social Linkedin"/></a>
+                    <a href="https://www.instagram.com/"><img src={instagram} alt="Logo da rede social Instagram"/></a>
+                </nav>
+            </section>
+            <section className={S.direitosAutorais}>
+                <p>Layout desenvolvido pela Vai Na Web para fins educativos - 2024</p>
             </section>
         </footer>
     )
